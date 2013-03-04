139e58bf2cb4a73992895fc57fbe9487ac52ce3b
Michael <seally.1186@gmail.com>
2013-03-04 13:23:03 -0800

    Untested DCellRouting.

--- routing.py
+++ routing.py
@@ -9,11 +9,21 @@ class DCellRouting(Routing):
   def get_route(self, src, dst, pkt):
     print "Route from %s to %s" % (src, dst)
     if src == dst: return [src]
+
     src_id = self.topo.id_gen(name = src)
     dst_id = self.topo.id_gen(name = dst)
+
     if src_id.prefix[:-1] == dst_id.prefix[:-1]:
-      prefix = src_id.prefix[:-1] + [0]
-      sw = self.topo.id_gen(prefix, DCellNodeID.SWITCH)
-      print "Local switch: %s" % sw
+      sw = self.topo.id_gen(src_id.prefix[:-1] + [0], DCellNodeID.SWITCH)
       return [src, sw.name_str(), dst]
-    return None
+
+    # TODO: DCell1 only
+    s = src_id.prefix[0]
+    d = dst_id.prefix[1]
+    assert s != d
+    if s < d: min = s; max = d
+    else: min = d; max = s
+
+    n1 = self.topo.id_gen([min, max - 1], DCellNodeID.HOST_SW)
+    n2 = self.topo.id_gen([max, min], DCellNodeID.HOST_SW)
+    return self.get_route(src, n1.name_str(), pkt) + self.get_route(n2.name_str(), dst, pkt)
