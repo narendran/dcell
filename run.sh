2919890ad04dabbc2f007bdff25b452e0a5c4486
Michael <seally.1186@gmail.com>
2013-03-12 17:55:06 -0700

    Plotting.

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ sudo ./topo.py &
 $POX riplpox.riplpox --topo=dcell,1 --routing=dcell
 
 echo Would plot now.
-python plot/plot_rate.py -f out/txrate.txt -i "54h-eth1|11h-eth(2|3)" -o plot.png --maxy=120
-#python plot/plot_rate.py -f out/txrate.txt -i "11h" -o plot.png --maxy=120
+python plot/plot_rate.py -f out/txrate.txt -i "54h-eth1" -o plot.png --maxy=120
+python plot/plot_rate.py -f out/txrate.txt -i "11h-eth(2|3)" -o plot2.png --maxy=120
